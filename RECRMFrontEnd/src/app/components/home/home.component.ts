ea0badf2ef419bc2fc53dda590c85a02a111b790
David Arnbrister <arnbristerdjr@gmail.com>
2025-05-14 21:57:50 -0400

    front end is given permission to access backend information. home page displays proper dobs

--- RECRMFrontEnd/src/app/components/home/home.component.ts
+++ RECRMFrontEnd/src/app/components/home/home.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactService } from '../../services/contact.service';
+import { Router } from '@angular/router';
+import { Contact } from '../../models/contact.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  public upcomingBdays: Contact[] = [];
+
+
+  constructor(private cServe : ContactService, private router : Router) {}
+  ngOnInit() : void {
+    if(!localStorage.getItem("jwt"))
+      this.router.navigate(['login']);
+
+    this.cServe.getUpcomingBirthdays().subscribe(
+      response => {
+        this.sleep(1500);
+        this.upcomingBdays = response;
+      },
+      error => {
+        console.warn("An error has occured");
+      }
+    );
+  }
+
+  sleep(ms: number | undefined) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
 
 }
